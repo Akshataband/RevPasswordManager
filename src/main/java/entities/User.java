a3f04d27e52ce499cafab99c49bb497613445fa5
Akshata <bandakshata@gmail.com>
2026-02-15 21:19:42 +0530

    Recreated project

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,34 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private String phone;
+    private boolean twoFactorEnabled;
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<PasswordEntry> passwordEntries;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<SecurityQuestion> securityQuestions;
+}
